dd9ff71590f7aca076419ebfdb3d36294a535e15
Anubhav Bhardwaj <anubhav.bhardwaj0222@gmail.com>
2026-08-09 11:19:43 +0530

    implemented cleaner logic solution04 loops

--- 03. Loops/04_Solution.py
+++ 03. Loops/04_Solution.py
@@ -5,7 +5,10 @@ string = input("Enter a string you want to get inverse of: ").strip()
 inverted_string = ""
 length = len(string)
 
-for i in range(length-1, -1, -1):
-    inverted_string += string[i]
+# for i in range(length-1, -1, -1):
+#     inverted_string += string[i]
+for char in string:
+    inverted_string = char + inverted_string  
 
 print(inverted_string)
+
