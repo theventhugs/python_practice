8031f181fb2e913fbccf78adf3af0eb5034a8bc6
Anubhav Bhardwaj <anubhav.bhardwaj0222@gmail.com>
2026-08-09 11:21:29 +0530

    implemented another logic solution05 loops

--- 03. Loops/05_Solution.py
+++ 03. Loops/05_Solution.py
@@ -5,3 +5,12 @@ string = input("Enter a string you want to get inverse of: ").strip()
 for index, char in enumerate(string):
     if char not in string[:index] and char not in string[index+1:]:
         print(char)
+
+
+# input_str = "teeteracdacd"
+
+# for char in input_str:
+#     print(char)
+#     if input_str.count(char) == 1:
+#         print("Char is: ", char)
+#         break
