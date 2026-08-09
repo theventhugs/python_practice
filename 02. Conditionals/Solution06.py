a7fa20464699e6b524be15e9a83c08a9e8af8c74
Anubhav Bhardwaj <anubhav.bhardwaj0222@gmail.com>
2026-08-09 08:57:46 +0530

    added unsolved solution06 conditionals

---
+++ 02. Conditionals/Solution06.py
@@ -0,0 +1,3 @@
+# Problem: Choose a mode of transportation based on the distance (e.g., <3 km: Walk, 3-15 km: Bike, >15 km: Car).
+
+# simmilar to previous questions, just get the distance from point A to B and use simple if else statements
